d68a0edbd6f4818c39f606984eb2464eb2c812eb
Jonas <jonas-focke@mailbox.org>
2022-07-07 16:01:45 +0200

    added routes to Menu

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -6,6 +6,12 @@ encapsules all the components in the header
   import MediaQuery from '$lib/components/Wrapper/MediaQuery.svelte';
   import InplacePopup from '$lib/components/Wrapper/InplacePopup.svelte';
 
+  let menuEntrys = [
+    { name: 'Lebenslauf', path: '/', selected: false },
+    { name: 'Technologien', path: '/technologies', selected: false },
+    { name: 'Interessen', path: '/interests', selected: false },
+    { name: 'Kontakt + Downloads', path: '/contact', selected: false }
+  ];
   let showMenu = false;
 </script>
 
@@ -14,17 +20,24 @@ encapsules all the components in the header
     <div class="absolute">
       <MediaQuery query="desktop">
         <div class="text-2xl mt-10">
-          <Menu orientation="horizontal" />
+          <Menu orientation="horizontal" {menuEntrys} />
         </div>
       </MediaQuery>
       <MediaQuery query="mobile">
-        <div on:click={() => (showMenu = !showMenu)} class="w-1/12">〰</div>
+        <div on:click={() => (showMenu = !showMenu)} class="w-1/12">
+          <i class="fa fa-bars text-accent text-4xl" />
+        </div>
         {#if showMenu}
           <InplacePopup on:outsideClick={() => (showMenu = !showMenu)}>
-            <Menu orientation="vertical" />
+            <Menu orientation="vertical" {menuEntrys} />
           </InplacePopup>
         {/if}
       </MediaQuery>
+    </div>
+    <div class="ml-auto">
+      <a href="https://github.com/JonasFocke01/Portfolio" target="_blank">
+        <i class="fa fa-github text-accent text-4xl" />
+      </a>
     </div>
   </div>
 </header>
