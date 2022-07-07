93717afceed2412026866bd488b37ed8d1f4bedf
Jonas <jonas-focke@mailbox.org>
2022-07-07 16:03:26 +0200

    refined site structure and build propper first home site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,5 @@
 <script lang="ts">
-  import Card from '$lib/components/Wrapper/Card.svelte';
-  import Text from '$lib/components/Wrapper/Text.svelte';
+  import Home from '$lib/sites/home/Home.svelte';
 </script>
 
-<div class="w-full flex justify-center">
-  <div class="w-1/3 mt-10">
-    <Card>
-      <div class="text-center">
-        <Text text="HELLO WORLD!" size="large" />
-      </div>
-      <div class="mt-4">
-        <Text
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-        minus dolores voluptatem, voluptates repellat ex quidem modi iusto
-        ratione architecto repellendus possimus blanditiis temporibus adipisci
-        quo, qui odit non voluptas!"
-        />
-      </div>
-    </Card>
-  </div>
-</div>
+<Home />
