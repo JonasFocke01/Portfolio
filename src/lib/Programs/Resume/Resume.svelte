3dbb66bf220aac48003bf99ae67083a622c3b12e
Jonas <jonas-focke@mailbox.org>
2022-07-10 12:26:50 +0200

    please stay

---
+++ src/lib/Programs/Resume/Resume.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+  import Text from '$lib/components/Wrapper/Text.svelte';
+  import { fly, fade } from 'svelte/transition';
+  import { cubicInOut } from 'svelte/easing';
+  import { onMount } from 'svelte';
+  import BeamItem from '$lib/Programs/Resume/BeamItem.svelte';
+  import MediaQuery from '$lib/components/Wrapper/MediaQuery.svelte';
+  import NormalButton from '$lib/components/Input/NormalButton.svelte';
+  import { createEventDispatcher } from 'svelte';
+  import Window from '../unilities/window.svelte';
+
+  const dispatcher = createEventDispatcher();
+  let displayHint: boolean = true;
+  let transitionStart = false;
+
+  onMount(() => {
+    transitionStart = true;
+  });
+</script>
+
+{displayHint}
+{#if transitionStart}
+  <div class="mt-4">
+    {#if displayHint}
+      <div
+        class="absolute -mt-4"
+        out:fade={{ duration: 500, easing: cubicInOut }}
+      >
+        <Text text="Klick mich" size="small" />
+      </div>
+    {/if}
+    <Window title="Anstellung bei der auto dm">
+      <Text text="Bei der auto dm" size="large" />
+      <div class="mt-2">
+        <Text
+          text="Bei der auto dm konnte ich mein errungenes Fachwissen vertiefen und neue Technologien erforschen. Hier kann ich meine Leidenschaft für SPA Frameworks wie Svelte voll entwickeln."
+        />
+      </div>
+    </Window>
+  </div>
+  <div class="mt-20">
+    <Window title="Ausbildung bei Beresa">
+      <Text text="Beresa GmbH & Co. KG" size="large" />
+      <div class="mt-2">
+        <Text
+          text="In der Ausbildung bei Beresa habe ich gelernt, wie ich mit Typescript arbeiten kann. Wie Git funktioniert, wie man Linux bedient, wie Javascript, HTML und CSS zusammenspielen, wie SQL Datenbanken laufen und wie man Anwendungen per REST-Schnittstelle anspricht. Ich konnte außerdem mein Fachwissen bezüglich Scum vertiefen. Ich durfte im Kundenkontakt, aber auch 'hinter dem Bildschirm' arbeiten."
+        />
+      </div>
+    </Window>
+  </div>
+  <div class="mt-20">
+    <Window title="Praktikum bei con terra">
+      <Text text="Praktikum bei con terra" size="large" />
+      <div class="mt-2">
+        <Text
+          text="Im Praktikum bei con terra habe ich meine Java Kenntnisse vertiefen können, indem wir dort eine eigenständige Anwendung entwickelt haben, die mit einer REST-Schnittstelle kommuniziert und so genaue Geo-Daten abruft. Ich konnte auch mein Fachwissen bezüglich Scum vertiefen."
+        />
+      </div>
+    </Window>
+  </div>
+  <div class="mt-20">
+    <Window title="Studium an der WWU" on:clicked={() => (displayHint = false)}>
+      <Text text="Westfälische Wilhelms Universität" size="large" />
+      <div class="mt-2">
+        <Text
+          text="An der WWU in Münster im Studiengang 'Geoinformatik' erfuhr ich tiefergehende Kenntnisse im Bereich der IT. Dort brachte man mir vor allem die Sprache Java bei, ich konnte mich aber auch mit logischen Problemen auseinandersetzen, so wie Themen rund um die Geologie unseres Planeten. "
+        />
+      </div>
+    </Window>
+  </div>
+  <div class="mt-20">
+    <Window title="Abitur an der JCS" on:clicked={() => (displayHint = false)}>
+      <Text text="Johann-Conrad-Schlaun" size="large" />
+      <div class="mt-2">
+        <Text
+          text="Meine Hochschulreife erhielt ich an der JCS Nordkirchen. Hier hatte ich erste Berührungspunkte mit dem Thema Datenbanken und Java Entwicklung."
+        />
+      </div>
+    </Window>
+  </div>
+  <!-- <div class="mt-64">
+          <Window
+            title="Sekundarstufe I"
+            on:clicked={() => (displayHint = false)}
+          >
+            
+              <Text text="Sekundarstufe I" size="large" />
+            <div class="mt-2">
+              <Text
+                text="Meine Schullaufbahn nahm ihren Anfang an der Gesamtschule Nordkirchen"
+              />
+            </div>
+          </Window>
+        </div> -->
+  <div class="mt-96">
+    <Window title="Ehemaligenverein der JCS">
+      <Text text="Ehemaligenverein" size="large" />
+      <div class="mt-2">
+        <Text
+          text="Nach Abschluss des Abiturs gründete ich, zusammen mit anderen engagierten Klassenkammeraden, den Verein 'Ehemaligenverein Johann-Conrad-Schlaun Nordkirchen'. Wir veranstallten regelmäßig Events für ehemalige Schüler, setzen uns aber auch für die Bildung der aktuellen Schüler ein, indem wir z.B. einen Wordkurs für Abiturienten oder eine Berufswahlmesse anbieten. Ich bin Vorstandsmitglied und für den Internetauftritt des Vereins mit verantwortlich: https://www.jcs-ehemaligenverein.de/"
+        />
+      </div>
+    </Window>
+  </div>
+  <div class="mt-20">
+    <Window title="Führerschein Klasse B">
+      <Text text="Führerschein" size="large" />
+      <div class="mt-2">
+        <Text text="Yay, ich darf auch Auto fahren :)" />
+      </div>
+    </Window>
+  </div>
+{/if}
