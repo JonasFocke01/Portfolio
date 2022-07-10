30d966ccfbf8d993c1ac94c54206ae9792cba640
Jonas <jonas-focke@mailbox.org>
2022-07-10 13:25:36 +0200

    altered card a bit

--- src/lib/components/Wrapper/Card.svelte
+++ src/lib/components/Wrapper/Card.svelte
@@ -4,6 +4,6 @@
 <script lang="ts">
 </script>
 
-<div class="bg-surface p-5 rounded-xl shadow-lg text-text text-2xl font-bold">
+<div class="bg-surface p-5 shadow-lg text-text text-2xl font-bold">
   <slot />
 </div>
