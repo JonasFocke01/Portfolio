2de68241455eb59e9ab6b025691ab589d5be858f
Jonas_Focke <jonas.focke@beresa.de>
2022-07-12 10:48:51 +0200

    init downloads

--- src/lib/Programs/Downloads.svelte
+++ src/lib/Programs/Downloads.svelte
@@ -1,22 +1,59 @@
 <script lang="ts">
   import Text from '$lib/components/Wrapper/Text.svelte';
+  import Fa from 'svelte-fa';
+  import {
+    faFileImage,
+    faFileArchive,
+    faFilePdf
+  } from '@fortawesome/free-solid-svg-icons';
+
+  const files = [
+    { name: 'Foto', icon: faFileImage, path: '/static/downloads/' },
+    {
+      name: 'Lebenslauf',
+      icon: faFilePdf,
+      path: '/static/downloads/Lebenslauf.pdf'
+    },
+    {
+      name: 'Arbeitszeugnis-Beresa',
+      icon: faFilePdf,
+      path: '/static/downloads/'
+    },
+    { name: 'Abitur-Zeugnis', icon: faFilePdf, path: '/static/downloads/' },
+    {
+      name: 'Bescheinigung-conterra',
+      icon: faFilePdf,
+      path: '/static/downloads/'
+    },
+    {
+      name: 'Bescheinigung-Ehemaligenverein',
+      icon: faFilePdf,
+      path: '/static/downloads/'
+    },
+    { name: 'Alle', icon: faFileArchive, path: '/static/downloads/' }
+  ];
+
+  function download(fileUrl: string, fileName: string) {
+    var a = document.createElement('a');
+    a.href = fileUrl;
+    a.setAttribute('download', fileName + '-Jonas_Focke');
+    a.click();
+    a.remove();
+  }
 </script>
 
-<div >
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
-  <Text text="Downloads" />
+<div class="m-2">
+  {#each files as file}
+    <div
+      class="flex flex-row space-y-2  hover:underline text-text cursor-pointer"
+      on:click={() => download(file.path, file.name)}
+    >
+      <div class="mt-3 text-text text-2xl">
+        <Fa icon={file.icon} />
+      </div>
+      <div class="ml-2">
+        <Text text={file.name} />
+      </div>
+    </div>
+  {/each}
 </div>
